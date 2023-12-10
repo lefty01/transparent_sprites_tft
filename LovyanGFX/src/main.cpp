7f55e525e3b322913d05fddb07fb3cc3c1413f57
Andreas Loeffler <aloeff@de.ibm.com>
2023-12-10 12:38:26 +0100

    remove startWrite (wrong usage\!). changed sprite object creation and pushSprite usage

--- LovyanGFX/src/main.cpp
+++ LovyanGFX/src/main.cpp
@@ -5,31 +5,18 @@
 
 #include <LovyanGFX.hpp>
 
-
-//#include "binaryttf.h"
-//#include "OpenFontRender.h"
-// #include "biomuell_90x50.h"
-// #include "papiermuell_90x50.h"
-// #include "restmuell_90x50.h"
-
-// #include <Ticker.h>
-// // 1 second tickers
-// #define TICKER_INTERVALL 1000
-// // min intervall 10 minutes, in the loop we some random time on top
-// #define MIN_INTERVALL 5 * 60
-// // Create a ticker
-// jsc::Ticker ticker_1sec(TICKER_INTERVALL);
-
-//WiFiClientSecure client;
-
 #include "rocket.h"
 
 
 LGFX tft;
-LGFX_Sprite bgSprite(&tft);
-LGFX_Sprite rocketSprite(&bgSprite);
-LGFX_Sprite txtSprite(&bgSprite);
-LGFX_Sprite txt2Sprite(&bgSprite);
+// LGFX_Sprite bgSprite(&tft);
+// LGFX_Sprite rocketSprite(&bgSprite);
+// LGFX_Sprite txtSprite(&bgSprite);
+// LGFX_Sprite txt2Sprite(&bgSprite);
+LGFX_Sprite bgSprite;
+LGFX_Sprite rocketSprite;
+LGFX_Sprite txtSprite;
+LGFX_Sprite txt2Sprite;
 
 
 //static auto transpalette = 0;
@@ -72,19 +59,6 @@ void setup() {
   rocketSprite.setSwapBytes(true);
   rocketSprite.pushImage(0, 0, 90, 90, rocket);
 
-  // Serial.print("Connecting to ");
-  // Serial.println(WIFI_SSID);
-  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  //   // timeout check ...
-  // }
-  // Serial.println("");
-  // Serial.println("WiFi connected.");
-
-
-  tft.startWrite();  // ?
 }
 
 uint n = 0;
@@ -101,14 +75,12 @@ void loop() {
   txt2Sprite.setTextFont(4);
   txt2Sprite.drawString(String(millisPerMove), 0, 0);
 
-  //  txtSprite.setTextFont(4);           // フォント種類を変更(時計盤の文字用)
-  //  txtSprite.setTextDatum(lgfx::middle_center);
 
 
-  rocketSprite.pushSprite(x, 20, TFT_BLACK);
-  txtSprite.pushSprite(140, 80, TFT_BLACK);
-  txt2Sprite.pushSprite(15, 100, TFT_BLACK);
-  bgSprite.pushSprite(0, 0);
+  rocketSprite.pushSprite(&bgSprite, x, 20, TFT_BLACK);
+  txtSprite.pushSprite(&bgSprite, 140, 80, TFT_BLACK);
+  txt2Sprite.pushSprite(&bgSprite, 15, 100, TFT_BLACK);
+  bgSprite.pushSprite(&tft, 0, 0);
 
 
   x++;
@@ -119,5 +91,5 @@ void loop() {
     Serial.println(millisPerMove);
   }
 
-  tft.display();  // ?
+  tft.display();
 }
